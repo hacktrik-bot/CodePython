343cf1a7b19a3e4e4043d00a36040388f1572079
ahujatarang <56203067+ahujatarang@users.noreply.github.com>
2019-10-06 16:02:00 +0530

    Created Sum_of_Two_Numbers.py

---
+++ Sum_of_Two_Numbers.py
@@ -0,0 +1,8 @@
+#Python program to add two numbers 
+  
+n1 = input("First number: ") 
+n2 = input("\nSecond number: ") 
+  
+sum = float(n1) + float(n2) 
+
+print("The sum of {0} and {1} is {2}" .format(n1, n2, sum)) 
