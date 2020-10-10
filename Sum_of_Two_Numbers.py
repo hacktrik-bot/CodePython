ee8aa780f97e2828c39a24e91b19b8a2b3596b42
Camilo Cunha de Azevedo <Camilotk@gmail.com>
2020-10-10 01:11:02 -0300

    removed main

--- Sum_of_Two_Numbers.py
+++ Sum_of_Two_Numbers.py
@@ -1,7 +1,3 @@
-def main():
-    n1 = int(input("First number: ")) 
-    n2  = int(input("Second number: ")) 
-    print("The sum of {0} and {1} is {2}" .format(n1, n2, (lambda a, b : a + b)(n1,n2) )) 
-
-if __name__ == "__main__":
-    main()
+n1 = int(input("First number: ")) 
+n2  = int(input("Second number: ")) 
+print("The sum of {0} and {1} is {2}" .format(n1, n2, (lambda a, b : a + b)(n1,n2) ))
