9e8199454616474f481f01fc5571ff92ece4b362
dany pm <daniel.pahlavan@gmail.com>
2022-08-09 19:30:12 +0430

    Update Hello_World.py
    +update to python3
    + add addition examples

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,31 @@
-print "Hello world!"
+
+#print "hello world" => In python2
+
+
+print ("Hello world!")
+
+
+#type 2 - use variable
+
+greeting = "Hello world!"
+
+print(greeting)
+
+#type 3 - use input 
+
+greeting = "Hello"
+print("Hello, who are you?")
+
+youtname = input()
+
+print(greeting , youtname ,"!")
+
+
+#type 4 - Use input without additional print.
+
+greeting = "Hello"
+
+
+youtname = input("Hello, who are you? ")
+
+print(greeting , youtname ,"!")
